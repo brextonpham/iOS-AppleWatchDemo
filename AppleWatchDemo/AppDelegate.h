dbc5e3dc1871b1b8918fdf0771867de061fa81b7
Brexton Pham <brextonpham@stanford.edu>
2015-07-06 12:23:39 -0400

    Initial Commit

---
+++ AppleWatchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleWatchDemo
+//
+//  Created by Brexton Pham on 7/6/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
